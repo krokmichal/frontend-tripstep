5a0c87d3b9f917547232e457ef61c9c7935bed05
Michał Krok <krokmichal1@gmail.com>
2025-05-23 15:02:08 +0200

    zmiana targetu w vite.config.js oraz cofniecie zmian w axios.js

--- src/axios.js
+++ src/axios.js
@@ -4,8 +4,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://backend-tripstep.onrender.com/api',
-  withCredentials: true,
+  baseURL: '/api',
 });
 
 instance.interceptors.request.use((config) => {
